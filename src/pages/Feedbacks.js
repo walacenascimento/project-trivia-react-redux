730c4c239ceef5dab3b3e14b9eaf9ae09925cdae
thomasravache <thomasravache31@gmail.com>
2021-09-06 15:20:38 -0300

    Implementando funçao handleClick

--- src/pages/Feedbacks.js
+++ src/pages/Feedbacks.js
@@ -4,6 +4,22 @@ import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedbacks extends Component {
+  constructor(props) {
+    super(props) {
+      this.handleClick = this.handleClick.bind(this);
+    }
+  }
+
+  handleClick(testId) {
+    const { history } = this.props;
+    if(testId === 'Ranking') {
+      history.push('/ranking');
+    } 
+    if (testId === 'Jogar Novamente!') {
+      history.push('/gamepage');
+    }
+  }
+
   render() {
     return (
       <div>
