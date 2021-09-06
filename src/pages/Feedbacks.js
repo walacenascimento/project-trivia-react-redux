577894307fea29daa5ca7bf9e7ebf5b1e58e7839
thomasravache <thomasravache31@gmail.com>
2021-09-06 15:58:47 -0300

    Finalizando tarefa: criando botões e implementando logica

--- src/pages/Feedbacks.js
+++ src/pages/Feedbacks.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import Button from '../components/Button';
 
 class Feedbacks extends Component {
   constructor(props) {
@@ -10,29 +11,71 @@ class Feedbacks extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(testId) {
+  handleClick({ target: { textContent } }) {
     const { history } = this.props;
-    if (testId === 'Ranking') {
+
+    if (textContent === 'Ver Ranking') {
       history.push('/ranking');
     }
-    if (testId === 'Jogar Novamente!') {
+
+    if (textContent === 'Jogar Novamente') {
       history.push('/gamepage');
     }
   }
 
   render() {
-    const { correctAnswers, score } = this.props;
+    const { correctAnswers, score } = this.props; // Vem do estado do Redux
     const WELL_PLAYED = 3;
+    const questionX = (correctAnswers === 1 ? 'questão' : 'questões');
 
     return (
       <div>
         <Header />
         <section>
-
+          <p data-testid="feedback-text">
+            {correctAnswers >= WELL_PLAYED ? 'Mandou bem!' : 'Poderia ser melhor...'}
+          </p>
+          <p data-testid="feedback-total-question">
+            {`Você acertou ${correctAnswers} ${questionX} de 5`}
+          </p>
+          <p data-testid="feedback-total-score">
+            {`Você marcou ${score} pontos no total!`}
+          </p>
+          <div className="button-container">
+            <Button
+              dataTestId="btn-play-again"
+              name="Jogar Novamente"
+              onClick={ this.handleClick }
+            />
+            <Button
+              dataTestId="btn-ranking"
+              name="Ver Ranking"
+              onClick={ this.handleClick }
+            />
+          </div>
         </section>
       </div>
     );
   }
 }
 
-export default Feedbacks;
+Feedbacks.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  score: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+  ]).isRequired,
+  correctAnswers: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+  ]).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  score: state.results.score,
+  correctAnswers: state.results.correctAnswers,
+});
+
+export default connect(mapStateToProps, null)(Feedbacks);
