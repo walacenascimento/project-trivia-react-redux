f902e3dd649b7e07658ecb270ca5f8939c6377d8
thomasravache <thomasravache31@gmail.com>
2021-09-06 14:38:56 -0300

    Arrumando linha 3 do triviaAPI.js

--- src/services/triviaAPI.js
+++ src/services/triviaAPI.js
@@ -1,6 +1,6 @@
 const fetchTriviaAPI = async () => {
   const fetchAPI = await fetch('https://opentdb.com/api_token.php?command=request');
-  const response = fetchAPI.json();
+  const response = await fetchAPI.json();
 
   return response;
 };
