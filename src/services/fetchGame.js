3c73b2f08e8c757b78ec9eed51c6b996d7be46f0
thomasravache <thomasravache31@gmail.com>
2021-09-09 16:04:25 -0300

    Finalizando implementaçao do fetchGame.js

--- src/services/fetchGame.js
+++ src/services/fetchGame.js
@@ -1,17 +1,35 @@
-// a
+import fetchTriviaAPI from './triviaAPI';
+
 const fetchQuestions = async (token, configs) => {
   const EXPIRED_CODE = 3; // Se for um token inválido é retornado código 3 na resposta
-  const { category, type, difficulty } = configs;
-  const END_POINT = `https://opentdb.com/api.php?encode=base64&amount=5&token=${token}&category=${category}&difficulty=${difficulty}&type=${type}&encode=base64`;
-  const fetch = await fetch(END_POINT);
-  const response = await fetch.json();
-  const {  }
+  const { category, difficulty, type } = configs;
+  const END_POINT = `https://opentdb.com/api.php?amount=5&token=${token}&category=${category}&difficulty=${difficulty}&type=${type}`;
+  const fetchQuestion = await fetch(END_POINT);
+  const response = await fetchQuestion.json();
+  const { results } = response;
 
-  if (response.response_code === EXPIRED_CODE) { // Verifica se o token é válido ou não
-    
-  };
+  if (response.response_code === EXPIRED_CODE) return 'token failure';
+
+  return results; // retorna um array com as questões, cada elemento do array é um objeto
 };
 
-export const mountQuestions = async () => {
+const mountQuestions = async (configs) => {
+  let token = localStorage.getItem('token');
 
+  if (!token) {
+    fetchTriviaAPI();
+    token = localStorage.getItem('token');
+  }
+
+  let mountedQuestions = await fetchQuestions(token, configs);
+
+  if (mountedQuestions === 'token failure') {
+    fetchTriviaAPI();
+    token = localStorage.getItem('token');
+    mountedQuestions = await fetchQuestions(token, configs);
+  }
+
+  return mountedQuestions;
 };
+
+export default mountQuestions;
