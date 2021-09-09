99806ee5d288a03323f963423ead89a2132c1ddd
Leme <andresleme@gmail.com>
2021-09-09 15:43:58 -0300

    [incompleto] next question button

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Question from '../components/Question';
+import Button from '../components/Button';
 
 class GamePage extends Component {
   constructor() {
@@ -10,11 +11,14 @@ class GamePage extends Component {
     this.state = {
       questions: [],
       questionsIndex: 0,
+      hidden: true,
       loading: true,
     };
     this.componentDidMount = this.componentDidMount.bind(this);
     this.getQuestions = this.getQuestions.bind(this);
+    this.hideNextQuestionButton = this.hideNextQuestionButton.bind(this);
     this.nextQuestion = this.nextQuestion.bind(this);
+    this.showNextQuestionButton = this.showNextQuestionButton.bind(this);
   }
 
   componentDidMount() {
@@ -30,25 +34,55 @@ class GamePage extends Component {
     });
   }
 
+  hideNextQuestionButton() {
+    this.setState({
+      hidden: true,
+    });
+  }
+
   nextQuestion() {
     this.setState((prevState) => ({
       questionsIndex: prevState.questionsIndex + 1,
     }));
   }
 
+  showNextQuestionButton() {
+    this.setState({
+      hidden: false,
+    });
+  }
+
   render() {
-    const { question, questionsIndex, loading } = this.state;
+    const { hidden, question, questionsIndex, loading } = this.state;
+    const { nextQuestion } = this;
     return (
       <>
         <Header />
+
         {
-          loading ? <p>Loading...</p> : <Question question={ question[questionsIndex] } />
+          loading ? <p>Loading...</p>
+            : <>
+              (
+              <Question
+                hide={ this.hideNextQuestionButton }
+                question={ question[questionsIndex] }
+                show={ this.showNextQuestionButton }
+              />
+              <Button
+                className=""
+                data-testid="btn-next"
+                disabled={ hidden }
+                name="Próxima"
+                onClick={ nextQuestion }
+              />
+              )
+              </>
         }
+
       </>
     );
   }
 }
-
 
 const mapStateToProps = ({ gameSettings: { difficulty, cattegory, type } }) => ({
   configs: {
@@ -61,8 +95,8 @@ const mapStateToProps = ({ gameSettings: { difficulty, cattegory, type } }) => (
 GamePage.propTypes = {
   configs: propTypes.shape({
     difficulty: PropTypes.string,
-  cattegory: PropTypes.string,
-  type: PropTypes.string,}),
+    cattegory: PropTypes.string,
+    type: PropTypes.string }),
 }.isRequired;
 
 export default connect(mapStateToProps)(GamePage);
